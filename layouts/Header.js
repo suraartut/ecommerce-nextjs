fc2b2af1131301610ec48b97ef00f7247fe5bc8e
Merve Şura ARTUT <suraartut@gmail.com>
2023-07-19 12:20:29 +0300

    update: Hamburger Menu

--- layouts/Header.js
+++ layouts/Header.js
@@ -6,8 +6,15 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { GrClose } from "react-icons/gr";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  const [showMenu, setshowMenu] = useState(false);
+
+  const handleClick = () => {
+    setshowMenu((prev) => !prev);
+  };
+
   const [category, setCategory] = useState([]);
 
   const Router = useRouter();
@@ -33,8 +40,24 @@ const Header = () => {
 
   return (
     <header className="w-full h-auto z-[100] bg-white relative">
-      <div className="flex px-20 py-5 w-full m-auto justify-between items-center uppercase text-[#141414]">
-        <div className="flex w-1/3 justify-around">
+      <div className="flex lg:px-20 px-6 py-5 w-full m-auto justify-between lg:items-center uppercase text-[#141414]">
+        <div class="flex lg:hidden relative">
+          <div className="flex">
+            {!showMenu ? (
+              <div class="space-y-1.5" onClick={handleClick}>
+                <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
+                <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
+                <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
+              </div>
+            ) : (
+              <AiOutlineClose
+                className="w-8 h-6 text-gray-600 animate-pulse"
+                onClick={handleClick}
+              />
+            )}
+          </div>
+        </div>
+        <div className="lg:flex hidden w-1/3 justify-around">
           {category?.map((item, index) => {
             if (index < 5) {
               return (
@@ -49,7 +72,7 @@ const Header = () => {
             }
           })}
         </div>
-        <div className="logo absolute left-[47%]">
+        <div className="logo absolute lg:left-[47%] left-[35%]">
           <Link href="/">
             <Image src="/logo.svg" width={112} height={21} alt="Company Logo" />
           </Link>
@@ -124,6 +147,26 @@ const Header = () => {
           </div>
           <HiOutlineShoppingBag className="text-2xl hover:text-gray-400 cursor-pointer" />
         </div>
+      </div>
+
+      <div className="deneme">
+        {showMenu && (
+          <div className="p-6">
+            {category?.map((item, index) => {
+              if (index < 5) {
+                return (
+                  <div key={item.id} className="p-3">
+                    <Link href="/">
+                      <div className="hover:text-gray-400 text-xl duration-300 transition-all ease-in-out">
+                        {item.name}
+                      </div>
+                    </Link>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        )}
       </div>
     </header>
   );
